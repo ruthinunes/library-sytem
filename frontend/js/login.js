0785877505dd180763077380cb2f7bdb8c446368
ruthinunes <ruthinunesdev@gmail.com>
2024-02-07 10:23:25 -0300

    Update login for cleaner code

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,39 +1,36 @@
-const email = document.getElementById("email");
-const pass = document.getElementById("password");
-
-const fetchDataLogin = async () => {
-  const res = await fetch("../data.json");
-  const data = await res.json();
-  return data.data.login;
-};
-
 const setButtonClick = () => {
   const formBtn = document.getElementById("formButton");
 
   formBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    setUser();
+    handleButtonClick();
   });
 };
 
-const setUser = async () => {
-  const dataLogin = await fetchDataLogin();
+const handleButtonClick = () => {
+  const email = document.getElementById("email").value;
+  const pass = document.getElementById("password").value;
 
-  getUser(dataLogin);
+  setUser(email, pass);
 };
 
-const getUser = (dataLogin) => {
+const setUser = async (email, pass) => {
+  const dataLogin = await fetchDataLogin();
+
   for (i = 0; i < dataLogin.length; i++) {
-    if (
-      email.value == dataLogin[i].email &&
-      pass.value == dataLogin[i].password
-    ) {
+    if (email == dataLogin[i].email && pass == dataLogin[i].password) {
       setLocalStorage(dataLogin[i].name);
       window.open("../pages/home.html", "_self");
       return;
     }
   }
   window.alert("[ERRO] e-mail ou senha incorretos!");
+};
+
+const fetchDataLogin = async () => {
+  const res = await fetch("../data.json");
+  const data = await res.json();
+  return data.data.login;
 };
 
 const setLocalStorage = (user) => {
